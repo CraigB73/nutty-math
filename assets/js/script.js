07ff27673518bc69db5decb0e7d82bf719924f3d
CraigB73 <craigbritton73@gmail.com>
2023-11-09 20:48:18 +0100

    Fix style: game section container

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,15 +28,16 @@ function handleNameInput(){
     const firstName = document.getElementById('fname');
     const fname = nameInput.value;
     const nameToLowerCase = fname.toLowerCase();
-    const nameFirstLetrCap = nameToLowerCase.charAt(0).toUpperCase() + nameToLowerCase.slice(1);
-    firstName.innerText = ` ${nameFirstLetrCap}`
-    gameValues.playerName = nameFirstLetrCap
+    const firstLetterCap = nameToLowerCase.charAt(0).toUpperCase() + nameToLowerCase.slice(1);
+    firstName.innerText = ` ${firstLetterCap}`
+    gameValues.playerName =  firstLetterCap;
     storeToLocalStorage()
   }
 };
 
 handleNameInput();
 
+/** Handels challenge input amount and store to localStorage */
 function handleNumberInput(nameInput) {
   if(nameInput){
       gameValues.remainingMathQuestion = parseInt(nameInput.value)
@@ -44,3 +45,38 @@ function handleNumberInput(nameInput) {
       storeToLocalStorage()
       }
 }
+
+// Waits until content in loaded to the dom.
+document.addEventListener('DOMContentLoaded', function() {
+  console.log(gameValues);
+  let buttons = document.getElementsByTagName('button');
+  const checkAnsButton = document.getElementById('checkAnswerButton');
+  playButton ? playButton.style.display = 'none' :  null // Check if playButton is loaded.
+  checkAnsButton && resetButton ? resetButton.insertAdjacentElement('beforebegin', checkAnsButton) : null;
+  for (let button of buttons) {
+    button.addEventListener('click', function() {
+      if(this.getAttribute('data-type') === 'submit-game-values'){
+        formSubmit()
+      }else{
+        throw `Error! Check that you have enter name and challenge amount.`
+      
+      }
+    })
+  };
+});
+
+/**
+ * Navigates to game page and displays
+ * intial gameValues: name, number math question, and first math question.
+ * */
+function formSubmit(event) {
+  event.preventDefault()
+  location.href = "game.html";
+  displayNewQuestion()
+};
+
+
+
+// Assigns and distructure getFromLocalStorge function and checks truthy value
+let storedValues = getFromLocalStorage();
+storedValues ? (gameValues = storedValues) : gameValues;
